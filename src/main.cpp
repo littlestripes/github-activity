1872b50d96ff2ac303faeab493467a925ac86df0
Elijah Kamali <94736197+littlestripes@users.noreply.github.com>
2024-12-29 15:49:35 -0600

    Implement JSON parsing functionality

--- src/main.cpp
+++ src/main.cpp
@@ -297,7 +297,80 @@ struct Event {
  * @return          A vector containing Events.
  */
 std::vector<Event> parse_json_response(const std::string& response) {
-    return std::vector<Event>();
+    using json = nlohmann::json;
+
+    std::vector<Event> events;
+
+    if (json::accept(response)) {
+        // valid JSON
+        const json response_json = json::parse(response);
+
+        if (response_json.contains("status") && response_json["status"] == "404") {
+            // user not found
+            std::cerr << "Error: user not found" << std::endl;
+            return std::vector<Event>();  // return a blank vector
+        } else {
+            for (auto it : response_json) {
+                Event new_event = {
+                    it["type"],
+                    it["created_at"],
+                    it["repo"]["name"],
+                    std::nullopt,
+                    std::nullopt,
+                    std::nullopt,
+                    std::nullopt,
+                    std::nullopt,
+                    std::nullopt,
+                    std::nullopt,
+                    std::nullopt,
+                    std::nullopt
+                };
+
+                // check for optional fields and set them if available
+                if (it["payload"].contains("action")) {
+                    new_event.action = it["payload"]["action"];
+                    if (std::strcmp(new_event.action.value().c_str(), "assigned") == 0) {
+                        new_event.assignee = it["payload"]["assignee"]["login"];
+                    }
+                }
+                if (it["payload"].contains("issue")) {
+                    new_event.issue_number = it["payload"]["issue"]["number"];
+                }
+                if (it["payload"].contains("member")) {
+                    new_event.collaborator = it["payload"]["member"]["login"];
+                }
+                if (it["payload"].contains("label")) {
+                    new_event.label = it["payload"]["label"]["name"];
+                }
+                if (it["payload"].contains("pull_request")) {
+                    new_event.pr_title = it["payload"]["pull_request"]["title"];
+                    new_event.pr_number = it["payload"]["pull_request"]["number"];
+                    if (it["payload"]["pull_request"].contains("requested_reviewers")) {
+                        std::vector<std::string> usernames;
+                        const auto& reviewer_objects_json = it["payload"]["pull_request"]["requested_reviewers"];
+
+                        // small performance boost
+                        usernames.reserve(reviewer_objects_json.size());
+
+                        for (const auto& user : reviewer_objects_json) {
+                            usernames.push_back(user["login"]);
+                        }
+                    }
+                }
+                if (it["payload"].contains("commits")) {
+                    new_event.commit_count = it["payload"]["commits"].size();
+                }
+
+                events.push_back(new_event);
+            }
+        }
+    } else {
+        // invalid json, return a blank vector
+        std::cerr << "Error: invalid JSON response" << std::endl;
+        return std::vector<Event>();
+    }
+
+    return events;
 }
 
 /**
@@ -337,6 +410,9 @@ int main(const int argc, const char* argv[]) {
 
         const std::string response_data = get_json_response(endpoint);
         const std::vector<Event> events = parse_json_response(response_data);
+
+        for (const Event& event : events)
+            std::cout << "- " << event.to_str() << std::endl;
     } catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
         std::cout << options.help() << std::endl;
